47768033ebf61b9df6e62db003f97ea4f5e3c199
rakatoneco ‎ <RakhaTaufiqMahardika.rakataufiq7@gmail.com>
2022-03-14 05:51:49 +0700

    praktikum 1 react

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Bmi from "./pages/bmi";
+import Bank from "./pages/bank";
+import Binary from "./pages/binary";
+import Final from "./pages/final";
+import Octal from "./pages/octal";
+import Decimal from "./pages/decimal";
+import Hexadecimal from "./pages/hexadecimal";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Octal/>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
